7142b0fd8a13fa83de3c14e76235efaf3cffcc51
Edvinas Beinartas <edvinas.beinartas@gmail.com>
2024-12-02 21:52:31 +0000

    Refactor: extract and simplify

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,75 +1,42 @@
 import kotlin.math.abs
 
 fun main() {
-//    The levels are either all increasing or all decreasing.
-//    Any two adjacent levels differ by at least one and at most three.
+    fun prepareInput(input: List<String>): List<List<Int>> =
+        input.map { it.split("""\s+""".toRegex()).map(String::toInt) }
 
-    fun part1(input: List<String>): Int {
-
-        val reports = input.map { it.split("""\s+""".toRegex()).map(String::toInt) }
-        var safe = 0
-
-        reports.forEach { report ->
-                var isIncreasing: Boolean? = null
-                val isSafe = report.zipWithNext { a, b ->
-                        if (isIncreasing == null) {
-                            isIncreasing = b > a
-                        }
-                        val diff = abs(b - a)
-                    !((isIncreasing != b > a) || (diff < 1 || diff > 3))
-                }.contains(false).not()
-
-                if (isSafe) {
-                    safe++
-                }
-        }
-        return safe
+    // The levels are either all increasing or all decreasing.
+    // Any two adjacent levels differ by at least one and at most three.
+    fun checkSafety(report: List<Int>): Boolean {
+        var isIncreasing: Boolean? = null
+        return report.zipWithNext { a, b ->
+            if (isIncreasing == null) {
+                isIncreasing = b > a
+            }
+            val diff = abs(b - a)
+            !((isIncreasing != b > a) || (diff < 1 || diff > 3))
+        }.contains(false).not()
     }
 
-    fun part2(input: List<String>): Int {
-        val reports = input.map { it.split("""\s+""".toRegex()).map(String::toInt) }
-        var safe = 0
-
-        fun checkSafety(report: List<Int>): Boolean {
-            var isIncreasing: Boolean? = null
-            return report.zipWithNext { a, b ->
-                if (isIncreasing == null) {
-                    isIncreasing = b > a
-                }
-                val diff = abs(b - a)
-                !((isIncreasing != b > a) || (diff < 1 || diff > 3))
-            }.contains(false).not()
-        }
-
-        reports.forEach { report ->
-            var isSafe = checkSafety(report)
-            if (!isSafe){
-                for (i in 0 until report.size){
-                    val damper = report.toMutableList()
-                    damper.removeAt(i)
-                    isSafe = checkSafety(damper)
-                    if (isSafe){
-                        break
-                    }
-                }
-            }
-
-
-            if (isSafe) {
-                safe++
-            }
-        }
-        return safe
+    fun part1(reports: List<List<Int>>): Int {
+        return reports.count { checkSafety(it) }
     }
 
+    // Damper the report by removing one level and check if it's still safe.
+    fun part2(reports: List<List<Int>>): Int {
+        return reports.count { report ->
+            checkSafety(report) || report.indices.any { i ->
+                checkSafety(report.toMutableList().apply { removeAt(i) })
+            }
+        }
+    }
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day02_test")
+    val testInput = prepareInput(readInput("Day02_test"))
     check(part1(testInput) == 2)
     check(part2(testInput) == 4)
 
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day02")
+    val input = prepareInput(readInput("Day02"))
     part1(input).println()
     part2(input).println()
 }
+
+

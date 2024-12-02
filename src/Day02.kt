8aa10cf2484a95015ee5d62bbe8bd9152ff25a66
Edvinas Beinartas <edvinas.beinartas@gmail.com>
2024-12-02 18:14:18 +0000

    Ugly & messy first solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,21 +1,75 @@
+import kotlin.math.abs
+
 fun main() {
+//    The levels are either all increasing or all decreasing.
+//    Any two adjacent levels differ by at least one and at most three.
+
     fun part1(input: List<String>): Int {
-        return input.size
+
+        val reports = input.map { it.split("""\s+""".toRegex()).map(String::toInt) }
+        var safe = 0
+
+        reports.forEach { report ->
+                var isIncreasing: Boolean? = null
+                val isSafe = report.zipWithNext { a, b ->
+                        if (isIncreasing == null) {
+                            isIncreasing = b > a
+                        }
+                        val diff = abs(b - a)
+                    !((isIncreasing != b > a) || (diff < 1 || diff > 3))
+                }.contains(false).not()
+
+                if (isSafe) {
+                    safe++
+                }
+        }
+        return safe
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val reports = input.map { it.split("""\s+""".toRegex()).map(String::toInt) }
+        var safe = 0
+
+        fun checkSafety(report: List<Int>): Boolean {
+            var isIncreasing: Boolean? = null
+            return report.zipWithNext { a, b ->
+                if (isIncreasing == null) {
+                    isIncreasing = b > a
+                }
+                val diff = abs(b - a)
+                !((isIncreasing != b > a) || (diff < 1 || diff > 3))
+            }.contains(false).not()
+        }
+
+        reports.forEach { report ->
+            var isSafe = checkSafety(report)
+            if (!isSafe){
+                for (i in 0 until report.size){
+                    val damper = report.toMutableList()
+                    damper.removeAt(i)
+                    isSafe = checkSafety(damper)
+                    if (isSafe){
+                        break
+                    }
+                }
+            }
+
+
+            if (isSafe) {
+                safe++
+            }
+        }
+        return safe
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 4)
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
 }

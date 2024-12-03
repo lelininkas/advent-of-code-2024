b334ed2ec78a98d5f621f730f4250284d6e4a470
Edvinas Beinartas <edvinas.beinartas@gmail.com>
2024-12-03 07:01:58 +0000

    Messy first try. Still need to sleep..

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,18 +1,52 @@
-
-
 fun main() {
+
+    // mul(ddd,ddd)
     fun part1(input: List<String>): Int {
-        return input.size
+        val regex = """(mul\()([0-9]{1,3})(,)([0-9]{1,3})(\))""".toRegex()
+        var result = 0
+
+        input.forEach {
+            val matches = regex.findAll(it)
+            for (match in matches) {
+                val (_, a, _,b) = match.destructured
+//                println(a)
+//                println(b)
+                result += a.toInt() * b.toInt()
+            }
+        }
+
+        return result
     }
 
+    // The do() instruction enables future mul instructions.
+    // The don't() instruction disables future mul instructions.
+    // Only the most recent do() or don't() instruction applies. At the beginning of the program, mul instructions are enabled.
     fun part2(input: List<String>): Int {
-        return input.size
+        val regex = """do\(\)|don't\(\)|(mul\()([0-9]{1,3})(,)([0-9]{1,3})(\))""".toRegex()
+        var result = 0
+        var enabled = true
+        input.forEach {
+            val matches = regex.findAll(it)
+            for (match in matches) {
+                if (match.value == "do()") {
+                    enabled = true
+                } else if (match.value == "don't()") {
+                    enabled = false
+                } else if (enabled) {
+                    val (_, a, _,b) = match.destructured
+                    result += a.toInt() * b.toInt()
+                }
+            }
+        }
+
+        return result
     }
 
-    val testInput = readInput("Day03_test")
-    check(part1(testInput) == 161)
+    check(part1(readInput("Day03_test")) == 161)
+    check(part2(readInput("Day03_test_pt2")) == 48)
 
+    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day03")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }

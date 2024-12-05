af46d3d68ccd1f537184802426f3163e06238d96
Edvinas Beinartas <edvinas.beinartas@gmail.com>
2024-12-05 05:42:10 +0000

    important info comments

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,4 +1,7 @@
 fun main() {
+    // Of course, you'll need to be careful: the actual list of page ordering rules is
+    // bigger and more complicated than the above example.
+    // Return sum of middle numbers of correct lines
     fun part1(input: List<String>): Int {
         return input.size
     }
@@ -7,7 +10,7 @@ fun main() {
 //        return input.size
 //    }
 
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day05_test")
     check(part1(testInput) == 143)
 
 //    val input = readInput("Day01")
